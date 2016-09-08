b1353d0ee97349f44c6de553923549d73d2322e5
artembloom <artembloom@gmail.com>
2016-09-08 10:50:41 +0300

    reduce boilerplate, add App component

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+import { Text } from 'react-native'
+
+class App extends Component {
+  render() {
+    return (
+      <Text>Hello, 0.0.1!</Text>
+    )
+  }
+}
+
+export default App
