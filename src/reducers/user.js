44ee963b99d4ac874f2327ae70736ba5ac4429b5
artembloom <artembloom@gmail.com>
2016-09-11 18:28:00 +0300

    change user reducer to handle user actions

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,10 +1,36 @@
+import * as types from '../actions/actionTypes'
+
 const initialState = {
-  isLoggedIn:  false,
-  authorizationToken: false
+  isLoggedIn:  false
 }
 
 const user = (state = initialState, action) => {
-  return state
+
+  switch (action.type) {
+
+    case types.LOGIN_FACEBOOK_SUCCESS:
+      return {
+        ...state,
+        isLoggedIn: false,
+        facebook: action.payload.facebook
+      }
+
+    case types.AUTH_USER_SUCCESS:
+      return {
+        ...state,
+        isLoggedIn: true,
+        authorizationToken: action.payload.authorizationToken
+      }
+
+    case types.AUTH_USER_ERROR:
+    case types.LOGIN_FACEBOOK_CANCELLED:
+    case types.LOGIN_FACEBOOK_ERROR:
+      return initialState
+
+    default:
+      return state
+
+  }
 }
 
 export default user
