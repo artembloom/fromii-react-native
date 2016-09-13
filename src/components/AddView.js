329c1193cba489fb37bc4776c552915fe9f55a87
artembloom <artembloom@gmail.com>
2016-09-13 22:33:36 +0300

    add AddView

---
+++ src/components/AddView.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+import {
+  View,
+  Text
+} from 'react-native'
+
+class AddView extends Component {
+  render() {
+    return (
+      <View>
+        <Text>This is AddView</Text>
+      </View>
+    )
+  }
+}
+
+export default AddView
