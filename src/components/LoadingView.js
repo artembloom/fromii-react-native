3fb3872c1ea0c8c3064698cd35fd2d7098940e99
artembloom <artembloom@gmail.com>
2016-09-13 21:29:49 +0300

    add Login, Profile and Loading components

---
+++ src/components/LoadingView.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+
+import * as theme from './theme'
+
+import {
+  ActivityIndicator,
+  View
+} from 'react-native'
+
+class LoadingView extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator
+          color={theme.COLOR_LINES}
+          size={'large'}
+        />
+      </View>
+    )
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: theme.COLOR_INVERSE,
+    alignItems: 'center',
+    justifyContent: 'center'
+  }
+}
+
+export default LoadingView
---
+++ src/components/LoginView/index.js
@@ -0,0 +1,72 @@
+import React, { Component, PropTypes } from 'react'
+import * as theme from '../theme'
+import LoadingView from '../LoadingView'
+import {
+  View,
+  TouchableOpacity,
+  Image,
+  Text
+} from 'react-native'
+
+class LoginView extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    console.log(this.props.user);
+    if (!this.props.user.facebook) {
+      return (
+        <View style={styles.container}>
+          <TouchableOpacity onPress={this.props.logIn}>
+            <View style={styles.button}>
+              <Image
+                source={require('./img/f.png')}
+                style={styles.icon}
+              />
+              <Text style={styles.caption}>
+                Log in with Facebook
+              </Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      )
+    }
+    else {
+      return <LoadingView />
+    }
+  }
+
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    backgroundColor: theme.COLOR_PRIMARY,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  button: {
+    flex: 0,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 16,
+    backgroundColor: 'transparent',
+    borderWidth: 1,
+    borderRadius: 8,
+    borderColor: theme.COLOR_INVERSE
+  },
+  icon: {
+    marginRight: 16,
+    height: 25,
+    width: 13
+  },
+  caption: {
+    color: theme.COLOR_INVERSE,
+    fontSize: 20
+  }
+}
+
+export default LoginView
