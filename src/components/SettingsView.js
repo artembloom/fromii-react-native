51f9584982096b0c6ccf099b44977055aaa87f27
artembloom <artembloom@gmail.com>
2016-09-20 21:20:54 +0300

    initial SettingsView

---
+++ src/components/SettingsView.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react'
+import {
+  View,
+  Text
+} from 'react-native'
+
+class SettingsView extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Settings</Text>
+      </View>
+    )
+  }
+}
+
+const styles = {
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+}
+
+export default SettingsView
